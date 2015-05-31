0979adfc3a6b5e98335c2ba80fafaf1aa30fdbbe
Paul Ageev <birne.ageev@gmail.com>
2015-05-31 11:13:46 +0500

    Temporary tests

---
+++ WindowsFormsTemp/Tests/JpegTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+using WindowsFormsTemp.ImagePrimitives;
+using WindowsFormsTemp.Jpeg;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class JpegTests
+    {
+        private const string DefaultImagePath = "ImageData/image_Lena256gb.bmp";
+
+        [Test]
+        public void SevenZipTest()
+        {
+            var bytes = new byte[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+            var encoded = SevenZipCoder.Instance.Encode(bytes);
+            foreach (var x in encoded)
+            {
+                Console.Write(x);
+                Console.Write(" ");
+            }
+
+            Assert.AreEqual(bytes, SevenZipCoder.Instance.Decode(SevenZipCoder.Instance.Encode(bytes)));
+        }
+
+        [Test]
+        public void Test()
+        {
+            IBitmap bitmap = new Bitmap(DefaultImagePath).ToPlainBitmap();
+
+            byte[] bytes = JpegCoder.Instance.Encode(bitmap);
+
+            Console.WriteLine(bytes.Length);
+        }
+    }
+}
---
+++ WindowsFormsTemp/Tests/TempTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class TempTests
+    {
+        private IEnumerable<int> ZigZag(int[,] matrix)
+        {
+            int n = matrix.GetLength(0);
+            for (int i = 0; i < n; ++i)
+            {
+                int x = 0;
+                int y = i;
+                while (y >= 0)
+                {
+                    yield return matrix[y, x];
+                    ++x;
+                    --y;
+                }
+            }
+            for (int i = 1; i < n; ++i)
+            {
+                int x = i;
+                int y = n - 1;
+                while (x < n)
+                {
+                    yield return matrix[y, x];
+                    ++x;
+                    --y;
+                }
+            }
+        }
+
+        [Test]
+        public void Test()
+        {
+            var matrix = new[,]
+            {
+                {1, 3, 6},
+                {2, 5, 9},
+                {4, 8, 12},
+            };
+
+            foreach (var x in ZigZag(matrix))
+            {
+                Console.Write(x);
+                Console.Write(" ");
+            }
+        }
+    }
+}
